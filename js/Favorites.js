0a7c85568edb3aad58d2a6cb1ba2159c58d0984a
Victor Zornek <vzornek@gmail.com>
2024-07-23 18:29:46 -0300

    add validação para não adicionar usuário duplicado e finalização do projeto

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,16 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint).then(data => data.json()).then(data => ({
-            login: data.login,
-            name: data.name,
-            public_repos: data.public_repos,
-            followers: data.followers
-        }))
-    }
-}
-
+import { GithubUser } from "./GithubUser.js"
 
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
@@ -33,6 +21,13 @@ export class Favorites {
 
     async add(username) {
         try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado!')
+            }
+
             const user = await GithubUser.search(username)
 
             if(user.login === undefined) {
