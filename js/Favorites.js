0b41c061da401e88cb432aae17edc31f5a1719ca
Victor Zornek <vzornek@gmail.com>
2024-07-23 18:10:05 -0300

    add a função para adicionar um usuário novo e função para salvar no local storage

--- js/Favorites.js
+++ js/Favorites.js
@@ -27,12 +27,34 @@ export class Favorites {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
     }
 
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+            const user = await GithubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries] //... serve para espalhar os usuários já cadastrados anteriormente nesse novo array
+            this.update()
+            this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
+    }
+
     delete(user) {  
         // Higher-order functions (map, filter, find, reduce)
         const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
 
         this.entries = filteredEntries
         this.update()
+        this.save()
 
         // feito dessa forma para respeitar o principio da imutabilidade
     }
@@ -48,6 +70,16 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
