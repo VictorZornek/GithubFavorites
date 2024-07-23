68c14ff184a1d1cbb7cac250d0de39b11cf3434a
Victor Zornek <vzornek@gmail.com>
2024-07-23 16:13:30 -0300

    add o delete de usuários

--- js/Favorites.js
+++ js/Favorites.js
@@ -25,6 +25,16 @@ export class Favorites {
         ]
 
     }
+
+    delete(user) {  
+        // Higher-order functions (map, filter, find, reduce)
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
+
+        // feito dessa forma para respeitar o principio da imutabilidade
+    }
 }
 
 
@@ -52,6 +62,16 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+
+                const isOk = confirm('Tem certeza que deseja deletar essa linhas?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+
+            }
 
             
             this.tbody.append(row)
